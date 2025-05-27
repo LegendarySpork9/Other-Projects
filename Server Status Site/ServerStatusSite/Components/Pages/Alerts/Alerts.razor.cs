43d41fcc23fd9febb0826fb81bcb5f7db658d935
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2025-05-27 09:03:40 +0100

    Alerts Refresh
    Added auto refreshing to the alerts page.

--- Server Status Site/ServerStatusSite/Components/Pages/Alerts/Alerts.razor.cs
+++ Server Status Site/ServerStatusSite/Components/Pages/Alerts/Alerts.razor.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
 using ServerStatusSite.Converters;
+using ServerStatusSite.Models;
 using ServerStatusSite.Models.API;
 using ServerStatusSite.Models.Data;
 using ServerStatusSite.Services;
+using System.Timers;
+using Timer = System.Timers.Timer;
 
 namespace ServerStatusSite.Components.Pages.Alerts
 {
@@ -13,17 +16,30 @@ namespace ServerStatusSite.Components.Pages.Alerts
         [Inject]
         private APIService APIService { get; set; }
         [Inject]
+        private AppSettingsModel AppSettings { get; set; }
+        [Inject]
         private NavigationManager Navigation { get; set; }
         [Inject]
         private UserModel User { get; set; }
         private APIAlertsModel ReportedAlerts = new();
+        private Timer RefreshTimer { get; set; }
         private int PageNumber = 1;
 
         protected override void OnInitialized()
         {
             Logger.LogMessage(StandardValues.LoggerValues.Info, "Opened Alerts Page");
 
+            RefreshTimer = new()
+            {
+                Interval = AppSettings.RefreshTime * 1000
+            };
+            RefreshTimer.Elapsed += (sender, e) => TimerElapsed(sender, e);
+
+            Logger.LogMessage(StandardValues.LoggerValues.Debug, $"Timer Duration: {RefreshTimer.Interval / 1000 / 60} minutes");
+
             ReportedAlerts = APIService.GetAlerts(PageNumber);
+
+            RefreshTimer.Start();
         }
 
         private string GetStyle(string component = null)
@@ -62,6 +78,11 @@ namespace ServerStatusSite.Components.Pages.Alerts
             {
                 Navigation.NavigateTo($"/editalert?alertId={alert.Id}");
             }
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            ReportedAlerts = APIService.GetAlerts(PageNumber);
         }
     }
 }
