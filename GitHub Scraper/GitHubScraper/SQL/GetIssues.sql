51bf7835d2238805ed1888dbe3aa094d8173f141
LegendarySpork9 <tobyhunter1999@btopenworld.com>
2026-06-09 20:25:45 +0100

    GitHub Scraper
    Migrated the GitHub Scraper to it's own repo.

--- GitHub Scraper/GitHubScraper/SQL/GetIssues.sql
+++
@@ -1,4 +0,0 @@
-select GitHubIssueId, DateCreated, DateSolved from Issue with (nolock)
-join Repository with (nolock) on Issue.RepositoryId = Repository.RepositoryId
-where [Name] = @repository
-order by IssueId asc
--- GitHub Scraper/GitHubScraper/SQL/GetLastRunDate.sql
+++
@@ -1,4 +0,0 @@
-select top 1 isnull(RunDate, '1900-01-01 00:00:00.000') from RunHistory with (nolock)
-join Repository with (nolock) on RunHistory.RepositoryId = Repository.RepositoryId
-where [Name] = @repository
-order by RunHistoryId desc
